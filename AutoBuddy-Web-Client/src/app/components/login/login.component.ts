f209bb53bfefef1e37bee81db1d7c3b9208ad658
Brandon McFarland <branmcf@gmail.com>
2018-03-29 18:48:23 -0500

    Added data binding for login and sign up.

--- AutoBuddy-Web-Client/src/app/components/login/login.component.ts
+++ AutoBuddy-Web-Client/src/app/components/login/login.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  public email: string;
+  public password: string;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  public submitLogin() {
+    console.log(this.email);
+    console.log(this.password);
+  }
+
+
 
 }
--- AutoBuddy-Web-Client/src/app/components/signup/signup.component.ts
+++ AutoBuddy-Web-Client/src/app/components/signup/signup.component.ts
@@ -9,7 +9,19 @@ export class SignupComponent implements OnInit {
 
   constructor() { }
 
+  public email: string;
+  public password1: string;
+  public password2: string;
+  public accounttype: number;
+
   ngOnInit() {
+  }
+
+  public submitSignup() {
+    console.log(this.email);
+    console.log(this.password1);
+    console.log(this.password2);
+    console.log(this.accounttype);
   }
 
 }
