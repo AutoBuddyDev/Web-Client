c17c2d393f64aa242c06f2dee6e45433bcfba819
Spencer Cheng <spencercheng@Spencers-MacBook-Pro.local>
2018-04-28 16:51:30 -0500

    navbar

--- AutoBuddy-Web-Client/src/app/components/hamburger/hamburger.component.ts
+++ AutoBuddy-Web-Client/src/app/components/hamburger/hamburger.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import {trigger, style, animate, transition} from '@angular/animations';
 @Component({
   selector: 'app-hamburger',
   templateUrl: './hamburger.component.html',
   styleUrls: ['./hamburger.component.css']
 })
 export class HamburgerComponent implements OnInit {
+  public nav:string;
+  openNav($event){
 
+    this.nav = $event.type == 'mouseenter' ? 'btn active' : 'btn';
+    console.log('help');
+  }
   constructor() { }
 
   ngOnInit() {
+    this.nav="btn";
   }
 
 }
