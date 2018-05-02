8e0180a9bf0bd096b317ae5e8ade131a18a2df36
Brandon McFarland <branmcf@gmail.com>
2018-05-01 20:06:10 -0500

    Added ngIfs to hamburger buttons and added logic to set controlling boolean based on the current route

--- AutoBuddy-Web-Client/src/app/components/hamburger/hamburger.component.ts
+++ AutoBuddy-Web-Client/src/app/components/hamburger/hamburger.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {trigger, style, animate, transition} from '@angular/animations';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError } from '@angular/router';
+import { checkAndUpdateView } from '@angular/core/src/view/view';
+
 @Component({
   selector: 'app-hamburger',
   templateUrl: './hamburger.component.html',
@@ -7,14 +10,45 @@ import {trigger, style, animate, transition} from '@angular/animations';
 })
 export class HamburgerComponent implements OnInit {
   public nav:string;
+  public isLoggedIn: boolean;
   openNav($event){
 
     this.nav = $event.type == 'mouseenter' ? 'btn active' : 'btn';
   }
-  constructor() { }
+  constructor(private router: Router) {
+    this.router = router;
+    router.events.subscribe( (event: Event) => {
+      if (event instanceof NavigationStart) {
+        this.checkNav();
+      }
+      if (event instanceof NavigationEnd) {
+        this.checkNav();
+      }
+      if (event instanceof NavigationError) {
+        this.checkNav();
+        console.log(event.error);
+      }
+  });
+}
 
   ngOnInit() {
     this.nav="btn";
+  }
+
+  public checkNav() {
+    console.log('Called Check Nav')
+    const loggedOutRoutes = [
+      '/landing',
+      '/login',
+      '/signup'
+    ];
+
+    if (loggedOutRoutes.includes(this.router.url)) {
+      this.isLoggedIn = false; 
+    }
+    else {
+      this.isLoggedIn = true;
+    }
   }
 
 }
